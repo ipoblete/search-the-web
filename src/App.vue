fd17d602aee4f9bd117b5570f3423eac136f799b
Ivan <i.poblete915@gmail.com>
2018-12-03 15:28:52 -0800

    wired up Home page

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </header>
+
+    <main>
+      <RouterView></RouterView>
+    </main>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-};
+export default {};
 </script>
 
 <style>
---
+++ src/components/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <section>
+    <h2>News Search</h2>
+    <!-- <router-link to="/home">search news</router-link> -->
+  </section>
+</template>
+
+<style>
+
+</style>
