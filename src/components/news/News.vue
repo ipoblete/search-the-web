3b8b9fd2b01219edaa82f72b96149587d4cc7e96
Ivan <i.poblete915@gmail.com>
2018-12-10 22:47:29 -0800

    setup Article Search

--- src/components/news/News.vue
+++ src/components/news/News.vue
@@ -2,7 +2,8 @@
   <section>
     <h2>News</h2>
     <!-- {{news}} -->
-    
+    <ArticleSearch :onSearch="handleSearch" :search="search"/>
+
     <div>
       <ul v-if="news">
         <Article v-for="(article, i) in news"
@@ -17,20 +18,27 @@
 <script>
 import api from '../../services/api';
 import Article from './Article';
+import ArticleSearch from './ArticleSearch';
 
 export default {
   data() {
     return {
-      news: null
+      news: null,
+      search: decodeURIComponent(this.$route.query.search),
     };
   },
   components: {
-    Article
+    Article,
+    ArticleSearch
   },
   created() {
     this.searchNews();
   },
   methods: {
+    handleSearch(search) {
+      this.search = search || '';
+      this.searchNews();
+    },
     searchNews() {
       api.getNews()
         .then(response => {
